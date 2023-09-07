f87681462744ddc14860cdcaa63c99910f125dc4
Musa-Aliu <127684266+Musa-Aliu@users.noreply.github.com>
2023-09-07 12:34:52 +0100

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -1,6 +1,1 @@
-from datetime import datetime
 
-name = input("What is your name? ")
-hour = datetime.now().time().hour
-greeting = "Good morning" if 4 <= hour < 12 else "Good afternoon" if 12 <= hour < 16 else "Good night"
-print(f"{greeting}, {name}!")
